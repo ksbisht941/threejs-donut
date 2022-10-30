25d5388b8e696286c4a56f269b666f9a211d53fd
ksbisht <ksbisht941@gmail.com>
2022-10-30 14:55:41 +0530

    revamp donut

--- js/script.js
+++ js/script.js
@@ -148,23 +148,6 @@ const sizes = {
 }
 
 /**
- * Resize
- */
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
  * Scroll
  */
 let scrollY = window.scrollY
